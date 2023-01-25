19d43b90c8fb5158e970a09df2f801afe348ce75
unknown <sashardoval@gmail.com>
2023-01-25 14:07:42 -0800

    cleaned up js page

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,4 @@
 
-//1) Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
 $(function () {   
     //turns the current hour into an integer
     var currentHours = dayjs().format('H');
@@ -27,15 +23,8 @@ $(function () {
       }
 
     }
-    
-
-    // 4) TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
 
     //assign localStorage to each individual time block by ID
-      
-    
 
       $('.saveBtn').on('click', function(){
         //user calendar value
@@ -51,16 +40,16 @@ $(function () {
         localStorage.setItem(eventKey, eventBox);
       })
       
-      $('#hour8 .description').val(localStorage.getItem('hour8'));
-$('#hour-9 textarea').val(localStorage.getItem('hour-9'));
-$('#hour-10 textarea').val(localStorage.getItem('hour-10'));
-$('#hour-11 textarea').val(localStorage.getItem('hour-11'));
-$('#hour-12 textarea').val(localStorage.getItem('hour-12'));
-$('#hour-13 textarea').val(localStorage.getItem('hour-13'));
-$('#hour-14 textarea').val(localStorage.getItem('hour-14'));
-$('#hour-15 textarea').val(localStorage.getItem('hour-15'));
-$('#hour-16 textarea').val(localStorage.getItem('hour-16'));
-$('#hour-17 textarea').val(localStorage.getItem('hour-17'));
+      //displays info from local storage even after refresh
+    $('#hour-9 textarea').val(localStorage.getItem('hour-9'));
+    $('#hour-10 textarea').val(localStorage.getItem('hour-10'));
+    $('#hour-11 textarea').val(localStorage.getItem('hour-11'));
+    $('#hour-12 textarea').val(localStorage.getItem('hour-12'));
+    $('#hour-13 textarea').val(localStorage.getItem('hour-13'));
+    $('#hour-14 textarea').val(localStorage.getItem('hour-14'));
+    $('#hour-15 textarea').val(localStorage.getItem('hour-15'));
+    $('#hour-16 textarea').val(localStorage.getItem('hour-16'));
+    $('#hour-17 textarea').val(localStorage.getItem('hour-17'));
 
     // Add code to display the current date in the header of the page.
     //calendar date
