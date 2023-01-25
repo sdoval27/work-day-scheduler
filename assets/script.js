b80646f2940f953514b958b2548dc2d1ffc6dfb1
unknown <sashardoval@gmail.com>
2023-01-25 14:04:09 -0800

    save button works!

--- assets/script.js
+++ assets/script.js
@@ -35,21 +35,33 @@ $(function () {
 
     //assign localStorage to each individual time block by ID
       
-
+    
 
       $('.saveBtn').on('click', function(){
+        //user calendar value
         var eventBox = $(this).siblings('textarea').val();
-        var eventKey = $('#hour-').children('.description');
+        //location of the user calendar value
+        var eventKey = $(this).parent().attr('id');
+        //save info to local storage
+        localStorage.getItem(eventKey, eventBox);
 
         console.log(eventBox);
         console.log(eventKey);
 
         localStorage.setItem(eventKey, eventBox);
-      
       })
+      
+      $('#hour8 .description').val(localStorage.getItem('hour8'));
+$('#hour-9 textarea').val(localStorage.getItem('hour-9'));
+$('#hour-10 textarea').val(localStorage.getItem('hour-10'));
+$('#hour-11 textarea').val(localStorage.getItem('hour-11'));
+$('#hour-12 textarea').val(localStorage.getItem('hour-12'));
+$('#hour-13 textarea').val(localStorage.getItem('hour-13'));
+$('#hour-14 textarea').val(localStorage.getItem('hour-14'));
+$('#hour-15 textarea').val(localStorage.getItem('hour-15'));
+$('#hour-16 textarea').val(localStorage.getItem('hour-16'));
+$('#hour-17 textarea').val(localStorage.getItem('hour-17'));
 
-      localStorage.getItem(eventKey, eventBox);
-    
     // Add code to display the current date in the header of the page.
     //calendar date
    var date = dayjs().format('dddd, MMMM D, YYYY h:mm A');
